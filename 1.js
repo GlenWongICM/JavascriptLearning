3ce01ec36404604081361f1c2f1d035bfaf080d3
GlenWongICM <138540350+GlenWongICM@users.noreply.github.com>
2023-07-18 12:13:06 +1000

    Update 1.js

--- 1.js
+++ 1.js
@@ -26,3 +26,26 @@ function helper(num) {
         return helper(num - 1) + helper(num - 2);
     }
 }
+
+
+function fibonacciGenerator(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    }
+    else {
+        output = [0, 1];
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+
+    }
+    return output;
+}
+
+
+
+
