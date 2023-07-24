c21efca9202ac2545d4a11f913826aba7afb939f
GlenWongICM <138540350+GlenWongICM@users.noreply.github.com>
2023-07-24 10:19:36 +1000

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 Node Read Eval Print Loop: in node it is like type node and enter. It will enter the node read eval print loop.
 Which is also called REPL
 
+Ctrl + c is a way to get out of command line interface..
 
 
   
